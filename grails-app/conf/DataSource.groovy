e245c0fe9593276dd1863e00431ef676ee6aa7af
Adan Gomez Quintanilla <small_los@hotmail.com>
2013-04-16 15:44:29 -0500

    dataSource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -6,38 +6,42 @@ dataSource {
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
+    cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
 environments {
     development {
-        dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:file:devDb"
-        }
+      dataSource {
+          dbCreate = "update"
+          driverClassName = "org.postgresql.Driver"
+          dialect = org.hibernate.dialect.PostgreSQLDialect
+      
+          //uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+          uri = new URI(System.env.DATABASE_URL?:"postgres://adnlos:postgres@localhost/electronica")
+
+          url = "jdbc:postgresql://"+uri.host+uri.path
+          username = uri.userInfo.split(":")[0]
+          password = uri.userInfo.split(":")[1]
+      }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
         }
     }
     production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
+      dataSource {
+          dbCreate = "update"
+          driverClassName = "org.postgresql.Driver"
+          dialect = org.hibernate.dialect.PostgreSQLDialect
+      
+          uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/electronica")
+
+          url = "jdbc:postgresql://"+uri.host+uri.path
+          username = uri.userInfo.split(":")[0]
+          password = uri.userInfo.split(":")[1]
+      }
     }
 }
